145685f004888024b2ac778be7075f7d671ff7c1
Joseph Keshet <joseph.keshet@biu.ac.il>
2014-07-16 11:04:26 +0300

    Name changing. Generate a script to compress the wav and tgs.

--- experiments/experiments_split.sh
+++ experiments/experiments_split.sh
@@ -1,8 +1,8 @@
-nohup run_over_many_files.sh 001 002 003 004 006 007 >& logs/run_over_many_files.001_007.log&
-nohup run_over_many_files.sh 008 009 010 011 012  >& logs/run_over_many_files.008_012.log&
-nohup run_over_many_files.sh 014 015 017 018 019 020 021 >& logs/run_over_many_files.014_021.log&
-nohup run_over_many_files.sh 022 023 024 025 026 028 >& logs/run_over_many_files.022_028.log&
-nohup run_over_many_files.sh 029 030 031 032 033 034 >& logs/run_over_many_files.029_034.log&
-nohup run_over_many_files.sh 035 036 037 038 039 >& logs/run_over_many_files.035_039.log&    
-nohup run_over_many_files.sh 040 041 042 043 046 >& logs/run_over_many_files.040_046.log&    
+nohup run_over_participants.sh 001 002 003 004 006 007 >& logs/run_over_participants.001_007.log&
+nohup run_over_participants.sh 008 009 010 011 012  >& logs/run_over_participants.008_012.log&
+nohup run_over_participants.sh 014 015 017 018 019 020 021 >& logs/run_over_participants.014_021.log&
+nohup run_over_participants.sh 022 023 024 025 026 028 >& logs/run_over_participants.022_028.log&
+nohup run_over_participants.sh 029 030 031 032 033 034 >& logs/run_over_participants.029_034.log&
+nohup run_over_participants.sh 035 036 037 038 039 >& logs/run_over_participants.035_039.log&    
+nohup run_over_participants.sh 040 041 042 043 046 >& logs/run_over_participants.040_046.log&    
 
---
+++ experiments/tar_participant_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tar cvzf data_001_007.tgz data/001* data/002* data/003* data/004* data/006* data/007*
+tar cvzf data_008_012.tgz data/008* data/009* data/010* data/011* data/012*  
+tar cvzf data_014_021.tgz data/014* data/015* data/017* data/018* data/019* data/020* data/021*
+tar cvzf data_022_028.tgz data/022* data/023* data/024* data/025* data/026* data/028*
+tar cvzf data_029_034.tgz data/029* data/030* data/031* data/032* data/033* data/034*
+tar cvzf data_035_039.tgz data/035* data/036* data/037* data/038* data/039*
+tar cvzf data_040_046.tgz data/040* data/041* data/042* data/043* data/046*
+
